5793c6ac1393aa2448311bb36e185b174960cac4
ashokDev10 <imashokdev@gmail.com>
2022-06-01 10:33:37 +0530

    getpost using id

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -2,10 +2,26 @@ const Post = require("../../models/Post");
 
 module.exports = {
   Query: {
+    // All POST
     async getPosts() {
       try {
         const posts = await Post.find();
         return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+
+    // SINGLE POST BASED ON ID
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
       } catch (err) {
         throw new Error(err);
       }
